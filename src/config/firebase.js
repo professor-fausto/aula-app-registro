548cf0ff85461d02bef3389308c38967f94d552f
professor-fausto <85196238+professor-fausto@users.noreply.github.com>
2022-06-02 17:48:42 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth"; // novidade
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC92Lxy4WtkqtvBZPTYh27SJpdJvw05nwE",
-    authDomain: "appregistro-d46a5.firebaseapp.com",
-    projectId: "appregistro-d46a5",
-    storageBucket: "appregistro-d46a5.appspot.com",
-    messagingSenderId: "767566334665",
-    appId: "1:767566334665:web:98c18d97ed03af3da9db18"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 export const app = initializeApp(firebaseConfig); // app vem daqui
